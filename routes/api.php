3821bbc543ea8af9f1bb88cdce9fd0bfa29f87b8
Danilo Augusto <dandevweb@gmail.com>
2024-10-26 15:31:24 -0300

    feat: Add login functionality with validation, token generation, and comprehensive tests for success and failure scenarios

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\{LoginController, RegisterController};
 
 Route::post('register', RegisterController::class)->name('register');
+Route::post('login', LoginController::class)->name('login');
