80eae7f8ad65a24e9fa6909f7b18af83324f8060
Danilo Augusto <dandevweb@gmail.com>
2024-10-26 15:35:36 -0300

    feat: Implement password reset functionality with validation, token handling, and tests for various success and failure cases

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\RecoveryPasswordController;
+use App\Http\Controllers\Auth\{RecoveryPasswordController, ResetPasswordController};
 use App\Http\Controllers\Auth\{LoginController, RegisterController};
 
 Route::post('register', RegisterController::class)->name('register');
 Route::post('login', LoginController::class)->name('login');
 Route::post('password-recovery', RecoveryPasswordController::class)->name('password.recovery');
+Route::put('password-reset', ResetPasswordController::class)->name('password.reset');
