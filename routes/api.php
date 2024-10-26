ccdc62ccf7b699141c3f67052ac41e6725e624d6
Danilo Augusto <dandevweb@gmail.com>
2024-10-26 15:34:26 -0300

    feat: Implement password recovery feature with email validation, token generation, and tests for successful and failed requests

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RecoveryPasswordController;
 use App\Http\Controllers\Auth\{LoginController, RegisterController};
 
 Route::post('register', RegisterController::class)->name('register');
 Route::post('login', LoginController::class)->name('login');
+Route::post('password-recovery', RecoveryPasswordController::class)->name('password.recovery');
